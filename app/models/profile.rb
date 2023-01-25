cbc5effdfb382da852997e5ba9e790875bcd3887
immprada <im.mprada@gmail.com>
2023-01-25 17:10:35 -0500

    add Profile model

---
+++ app/models/profile.rb
@@ -0,0 +1,8 @@
+class Profile < ApplicationRecord
+  belongs_to :user, dependent: :destroy
+
+  validates :username, presence: true, uniqueness: true, length: {
+    in: 3..10,
+    wrong_length: 'must be between 3 and 10 characters'
+  }
+end
