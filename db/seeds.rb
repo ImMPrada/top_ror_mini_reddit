1ef507d12913024bb3ac7953a54f6c436da6d9a1
immprada <im.mprada@gmail.com>
2023-01-26 17:31:36 -0500

    add seed for posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,34 @@ def build_username(nickname)
   nickname.downcase.split(' ').join
 end
 
+def build_a_quote
+  [
+    Faker::Quotes::Shakespeare.hamlet_quote,
+    Faker::Quotes::Shakespeare.as_you_like_it_quote,
+    Faker::Quotes::Shakespeare.king_richard_iii_quote,
+    Faker::Quotes::Shakespeare.romeo_and_juliet_quote
+  ].sample
+end
+
+def create_posts
+  puts 'creating posts...'
+
+  5.times do
+    profile = Profile.all.sample
+
+    puts "creating a post by #{profile.username}"
+
+    post = Post.create(
+      content: build_a_quote,
+      profile:
+    )
+
+    puts "post #{post.id} created!"
+  end
+
+  puts 'posts created!'
+end
+
 puts 'WANT TO ADD 10 MORE USERS?(yY/nN)'
 more_users = gets.chomp
 
@@ -35,8 +63,18 @@ if more_users.downcase == 'y' || User.all.empty?
     )
 
     puts "user #{user.id} created!"
-    puts "profile #{profile.id} profile created!"
+    puts "profile #{profile.id} created!"
   end
 
   puts 'users created!'
 end
+
+
+puts 'WANT TO MAKE 5 RANDOM POSTS FROM RANDOM USERS?(yY/nN)'
+more_posts = gets.chomp
+
+if more_posts.downcase == 'y' || Post.all.empty?
+  create_posts
+end
+
+
