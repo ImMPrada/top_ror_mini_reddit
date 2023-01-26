f741476606e8500e0d04ee9acd8fa671d0191acc
immprada <im.mprada@gmail.com>
2023-01-26 10:15:22 -0500

    add username validation to Profile model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,8 +1,13 @@
 class Profile < ApplicationRecord
   belongs_to :user, dependent: :destroy
 
-  validates :username, presence: true, uniqueness: true, length: {
-    in: 3..10,
-    wrong_length: 'must be between 3 and 10 characters'
-  }
+  validates :username, presence: true, uniqueness: true,
+                       length: {
+                         in: 3..10,
+                         wrong_length: 'must be between 3 and 10 characters'
+                       },
+                       format: {
+                         with: /[^-\s]/,
+                         message: "doesn't allow spaces"
+                       }
 end
