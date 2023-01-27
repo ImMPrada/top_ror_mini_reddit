2f9e8f327f59919473da2d6889e6e5b9cf3b97b7
immprada <im.mprada@gmail.com>
2023-01-26 22:51:03 -0500

    update User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_one :profile, dependent: :destroy
+  has_many :posts, through: :profile, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true, format: {
     with: URI::MailTo::EMAIL_REGEXP,
