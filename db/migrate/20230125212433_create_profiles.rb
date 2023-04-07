7b5d937c52f1d38017bfe56a29a92c499dedb8ae
miguel <26731448+ImMPrada@users.noreply.github.com>
2023-04-06 22:25:17 -0500

    Update db/migrate/20230125212433_create_profiles.rb
    Co-authored-by: Miguel Prada <mapra99@gmail.com>

--- db/migrate/20230125212433_create_profiles.rb
+++ db/migrate/20230125212433_create_profiles.rb
@@ -1,7 +1,7 @@
 class CreateProfiles < ActiveRecord::Migration[7.0]
   def change
     create_table :profiles do |t|
-      t.text :username, null: false
+      t.string :username, null: false
       t.belongs_to :user, null: false, foreign_key: true
 
       t.timestamps
